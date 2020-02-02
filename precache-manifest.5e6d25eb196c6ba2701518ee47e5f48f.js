566870641a4852c058659fa23a06a685503d5380
edymimolam <edymimolam@gmail.com>
2020-02-02 05:43:46 +0300

    Updates

---
+++ precache-manifest.5e6d25eb196c6ba2701518ee47e5f48f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f1c6bc113599c371f422f2320b0ec9",
+    "url": "/test-vk-api-react/index.html"
+  },
+  {
+    "revision": "4f3f6d3ef0f0679163c0",
+    "url": "/test-vk-api-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "82110c8c920c7f365d8a",
+    "url": "/test-vk-api-react/static/js/2.117ea9c0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/test-vk-api-react/static/js/2.117ea9c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f3f6d3ef0f0679163c0",
+    "url": "/test-vk-api-react/static/js/main.c4cbed32.chunk.js"
+  },
+  {
+    "revision": "d8ade8423766d4174478",
+    "url": "/test-vk-api-react/static/js/runtime-main.64d660f9.js"
+  }
+]);
